c8778230a50cec4136488886aedc88a06b217e84
pcs3rd <rhean1620@gmail.com>
2025-09-10 12:40:48 -0400

    Update database test, database library

--- sysmods/dataStore.py
+++ sysmods/dataStore.py
@@ -3,22 +3,22 @@ ABOUT THIS MODULE
 ----
 Author: Raymond Dean (KC3ZXI)
 Description: A class to help abstract database functions. 
-
-Provided Methods:
-> session(database name)
-> kill_session(session)
-> init_database_and_commit(session)
-> write_setting(session, setting, value)
-> read_setting(session, setting)
-> update_user_property(session, user id, property, value)
-> create_scavenger_hunt(session, )
 '''
 
-import sqlite3, datetime 
+import sqlite3, datetime, random, re
 
 class dataStore():
     # def __init__(self):
     #     pass
+    def sanitize_input(input): # Holy Crap. I'm done dealing with "-" garbo.
+        return re.sub("-", "_", input)
+
+    def randomwords(how_many=3):
+        one = ["crazy", "fast", "slow", "careful", "horse"]
+        two = ["horse", "cat", "dog", "pig", "barfs"]
+        three = ["jump", "nap", "sleep", "watch", "ponder"]
+        num = random.randrange(0,5)
+        return f"{one[random.randrange(0,5)]} {two[random.randrange(0,5)]} {three[random.randrange(0,5)]}"
 
     def session(name="default.db"):
         db = sqlite3.connect(name)
@@ -31,37 +31,35 @@ class dataStore():
     def init_database_and_commit(session):
         querry = [f'''
             CREATE TABLE IF NOT EXISTS settings (
-                id INTEGER PRIMARY KEY, 
-                setting text NOT NULL UNIQUE, 
+                setting text NOT NULL UNIQUE PRIMARY KEY, 
                 value text NOT NULL
             );''', 
             f'''
             CREATE TABLE IF NOT EXISTS user_data (
-                id INTEGER PRIMARY KEY, 
-                node_num int(9) NOT NULL, 
-                node_name varchar(36) NOT NULL,
+                node_num int(9) NOT NULL UNIQUE PRIMARY KEY, 
+                node_name varchar(36),
                 last_hit TEXT,
                 completed_hunts TEXT, 
                 current_hunt TEXT,
                 last_hunt TEXT, 
                 last_step_name TEXT, 
                 last_task_completion TEXT,
-                last_edit_ISO8601 TEXT NOT NULL,
+                last_edit TEXT NOT NULL,
                 last_edit_by int(9) NOT NULL
             );
             ''',
             f'''
             CREATE TABLE IF NOT EXISTS scavenger_hunts (
-                id INTERGER PRIMARY KEY, 
-                scavenger_name TEXT, 
+                scavenger_name TEXT UNIQUE PRIMARY KEY, 
                 scavenger_hunt_description TEXT,
-                scavenger_table TEXT, 
+                scavenger_table TEXT UNIQUE, 
                 scavenger_time_constrained BOOL, 
                 scavenger_start_time_ISO8601 TEXT, 
                 scavenger_end_time_ISO8601 TEXT,
                 scavenger_last_activity_ISO8601 TEXT,
                 scavenger_last_user TEXT,
-                scavenger_edit_ISO8601 TEXT
+                scavenger_last_edit_ISO8601 TEXT,
+                scavenger_by int(9)
             );
             '''
             ]
@@ -75,86 +73,101 @@ class dataStore():
 
 
     def write_setting(session, setting, value): # Used to update a setting property value
-        query = f'''
-            INSERT OR REPLACE INTO settings (setting, value) VALUES ('{setting}', '{value}');        
-        '''
+        query = f'''INSERT OR REPLACE INTO settings (setting, value) VALUES ('{setting}','{value}');'''
         session[1].execute(query)
         session[0].commit()
 
     def read_setting(session, setting): # Used to read a setting's property value
-        query = f'''
-            SELECT value FROM settings WHERE setting = '{setting}';
-        '''
+        query = f'''SELECT value FROM settings WHERE setting = '{setting}';'''
         session[1].execute(query)
         return session[1].fetchone()[0]
     
-    def update_user_property(session, user, property, value, editor='not provided'):
-        query = f'''
-            INSERT INTO user-data SET {property}='{value}' WHERE node_num='{user};'
-            UPDATE user-data SET last-edit_ISO8601 = '{datetime.datetime.now().isoformat()}' WHERE node_num='{user}';
-        '''
-        session[1].execute(command)
-        session[0].commit()
+    def write_user_property(session, user, property, value, editor='000000000'):
+        querry = [f'''INSERT OR REPLACE INTO user_data (node_num,{property},last_edit_by,last_edit) VALUES ({user},'{value}','{editor}','{datetime.datetime.now().isoformat()}');'''
+        ]
+        for command in querry:
+            session[1].execute(command)
+            session[0].commit()
 
+    def read_user_property(session, user, property):
+        query = f'''SELECT "{property}" FROM user_data WHERE node_num = '{user}';'''
+        session[1].execute(query)
+        return session[1].fetchone()[0]
+    
     # Scavenger Hunt Management
     # Need to update scavenger_hunts table. Work not finished yet.
-    def create_scavenger_hunt(cursor, request_user, scavenger_hunt_name=f"Scavenger_hunt-{datetime.datetime.now().isoformat()}"):
-        query = f'''
-            INSERT INTO scavenger-hunts (scavenger_name, created_by) ('{scavenger_hunt_name}', '{request_user}');
-
-
-            CREATE TABLE IF NOT EXISTS {scavenger_hunt_name} (
-                id INTERGER PRIMARY KEY,
-                name TEXT NOT NULL, 
+    def create_scavenger_hunt(session, scavenger_hunt_name=f"Scavenger_hunt {randomwords()}", request_user="000000000"):
+        query = [f'''
+            INSERT INTO scavenger_hunts (scavenger_name, scavenger_by, scavenger_table) VALUES ('{dataStore.sanitize_input(scavenger_hunt_name)}','{request_user}','{dataStore.sanitize_input(scavenger_hunt_name)}');
+            ''',
+            f'''
+            CREATE TABLE IF NOT EXISTS '{dataStore.sanitize_input(scavenger_hunt_name)}' (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 type TEXT,
                 required_previous_step BOOL,
-                enabled BOOL, 
                 previous_step_index INT,
                 hint TEXT, 
                 longitude NUMERICAL, 
                 latitude NUMERICAL, 
                 answer TEXT,
-                created_by int(9) NOT NULL
+                enabled BOOL
             );
-            UPDATE scavenger-hunts SET last_edit_ISO8601 = '{datetime.datetime.now().isoformat()}' WHERE scavenger_name='{scavenger_hunt_name}';
-        '''
-        session[1].execute(query)
-        cursor.commit()
+            ''', 
+            f'''
+            UPDATE scavenger_hunts SET scavenger_last_edit_ISO8601 = '{datetime.datetime.now().isoformat()}' WHERE scavenger_name='{dataStore.sanitize_input(scavenger_hunt_name)}';
+            ''', 
+        ]
+        try:
+            for command in query:    
+                session[1].execute(command)
+                session[0].commit()
+        except sqlite3.IntegrityError:
+            raise "scavenger creation has failed. Ensure name is unique"
 
-    def delete_scavenger_hunt(cursor, scavenger_hunt_name):
-        query = f'''
-            DROP TABLE {scavenger_hunt_name};
+
+    def delete_scavenger_hunt(session, scavenger_hunt_name):
+        query = [f'''
+            DROP TABLE {scavenger_hunt_name};''',
+            f'''
             DELETE FROM scavenger-hunts WHERE scavenger_name = '{scavenger_hunt_name}';
-            '''
-        session[1].execute(query)
+            ''']
+        for command in query:    
+            session[1].execute(command)
+            session[0].commit()
 
-    def modify_scavenger_hunt(cursor, scavenger_name, property, value):
+    def modify_scavenger_hunt(session, scavenger_name, property, value):
         query = f'''
-            INSERT OR REPLACE INTO settings SET {property} = '{value}' WHERE name='{scavenger_name};'
+            INSERT OR REPLACE INTO settings SET {property} = '{value}' WHERE name='{dataStore.sanitize_input(scavenger_name)};'
         '''
-        session[1].execute(query)
-        cursor.commit()        
+        session[1].execute(command)
+        session[0].commit()  
 
-    def list_scavenger_hunts(cursor):
+    def get_scavenger_hunts(session):
         query = f'''
-            SELECT name FROM scavenger-hunts'
+            SELECT scavenger_name FROM scavenger_hunts
         '''
         session[1].execute(query)
-        return cursor.fetchall()
+        return session[1].fetchall()
     
-    def get_all_scavenger_hunt_flows(cursor, scavenger_hunt_name):
+    def get_scavenger_steps(session, scavenger_hunt_name):
         query = f'''
-            SELECT * FROM {scavenger_hunt_name}' WHERE enabled=True
+            SELECT * FROM '{dataStore.sanitize_input(scavenger_hunt_name)}' WHERE enabled=True
         '''
         session[1].execute(query)
-        return cursor.fetchall()
+        return session[1].fetchall()
     
-    def add_scavenger_hunt_step(cursor, scavenger_name, property, value):
+    def write_scavenger_step_property(session, scavenger_name,  id, property, value):
         query = f'''
-            INSERT INTO {scavenger_name} SET {property} = '{value}'
+            #"SELECT rowid FROM components WHERE name = ?", (name,)
+            if empty
+            do:
+            INSERT OR REPLACE INTO {dataStore.sanitize_input(scavenger_name)} (id, {property}) VALUES ('{id}','{value}');
+            else: 
+            UPDATE {dataStore.sanitize_input(scavenger_name)} ({property}) VALUES ('{value}')
         '''
+            #INSERT INTO "{dataStore.sanitize_input(scavenger_name)}" SET {property} = '{value}'
         session[1].execute(query)
-        return cursor.fetchall()
+        session[0].commit()
 
                 # '''id INTERGER PRIMARY KEY,
                 # name TEXT NOT NULL, 
--- test_db.py
+++ test_db.py
@@ -1,4 +1,5 @@
-import sys, os, datetime
+import sys, os
+from datetime import datetime
 path = os.path.dirname(os.path.realpath(__file__)) + '/sysmods'
 if not path in sys.path:
     sys.path.insert(1, path)
@@ -16,16 +17,48 @@ print(f"----\nPython Path:\n{sys.path}\n----\n")
 
 from dataStore import dataStore
 
+now = datetime.now()
+formatted_date = now.strftime("%H%M%S")
+user_date = str(formatted_date)
 
 db = dataStore.session("test-db.db")
 
 print(db)
 
+
 print ("STEP: Test DB Configure")
 dataStore.init_database_and_commit(db)
 
-print("Test Setting Write: (test_setting = 'test success <time>')")
-dataStore.write_setting(db, 'test_setting', f'test success {datetime.datetime.now().isoformat()}')
+print(f"\nTest Setting Write")
+dataStore.write_setting(db, 'test_setting', f'test success {user_date}')
 
 print("STEP: Test Read Setting")
+print(f"\n---> Result should be 'test success {user_date}'")
 print(f"Test Setting is Value: {dataStore.read_setting(db, 'test_setting')}")
+
+print("STEP: Test User Data")
+dataStore.write_user_property(db, f"13{user_date}", "current_hunt", f"testHunt-temp")
+
+print("STEP: Read User Data")
+print(f"\n---> Result should be 'testHunt-{user_date}'")
+print(f"Test UserDat is Value: {dataStore.read_user_property(db, f"13{user_date}", "current_hunt")}")
+
+print("\n\n_______SECTION_______: Scavenger Data")
+dataStore.create_scavenger_hunt(db, f"testHunt-temp")
+print("\n_______SECTION_______: List Scavenger Hunts")
+for hunt in dataStore.get_scavenger_hunts(db):
+    print(hunt[0])
+
+print("\n_______SECTION_______: add Scavenger Hunt steps to test hunt")
+
+properties = {
+    "type":"loc",
+    "enabled":True,
+    "longitude":"12",
+    "latitude":"13"
+}
+for prop, value in properties.items():
+    dataStore.write_scavenger_step_property(db, f"testHunt-temp", 1, f"{prop}", f"{value}")
+
+print("\n_______SECTION_______: READ Scavenger Hunts")
+print(dataStore.get_scavenger_steps(db, "testHunt-temp"))
